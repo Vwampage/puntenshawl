ee5ef38aaa14bd625ee201c0aa3f5cc6c67c0285
David Michaels <dmichaels@signal.co>
2016-05-19 19:26:12 -0500

    Added a potential thesaurus dictionary, a json dictionary, and implemented a parser for the json dictionary. Will remedy the fact that it lacks the word 'POTATO' (possibly among others) later.

--- homophone_finder.py
+++ homophone_finder.py
@@ -2,14 +2,26 @@ import pronunciation_parser
 
 all_words = pronunciation_parser.create_dict(pronunciation_parser.open_dictionary())
 
-pun_on_this = "urine".upper()
+pun_on_this = "substantial".upper()
 print all_words[pun_on_this]
+
+def match_sounds(input_word,compare_word):
+	sound_matches = 0
+	for (sound1, sound2) in zip(all_words[pun_on_this], all_words[key]):
+		if sound1 == sound2:
+			sound_matches += 1
+	#Making this >= and also multiplying the length by some factor like 0.6 works, but it's not scientifically rigorous enough for now.
+	if sound_matches == len(all_words[pun_on_this]):
+		return True
+	else:
+		return False
+
+#def compare_length(input_word,compare_word):
+
 
 for key in all_words:
 	if len(all_words[key]) == len(all_words[pun_on_this]):
-		sound_matches = 0
-		for (sound1, sound2) in zip(all_words[pun_on_this], all_words[key]):
-			if sound1 == sound2:
-				sound_matches += 1
-		if sound_matches >= len(all_words[pun_on_this])*0.6:
+		if match_sounds(pun_on_this,key):
 			print key
+	#elif len
+
--- pronunciation_parser.py
+++ pronunciation_parser.py
@@ -1,8 +1,16 @@
+import json
+from pprint import pprint
+import xml.etree.ElementTree
+
+
+# class SemanticWord(object):
+# 	def __init__(word, definition, defnition_graph, pronunciation)
+
+
+
 def open_dictionary():
 	unparseddict = open('dictionary/cmudict.txt','r')
 	return unparseddict
-
-
 
 def create_dict(dictionary):
 	words = {}
@@ -11,9 +19,21 @@ def create_dict(dictionary):
 		words[word_tho[0]] = word_tho[2:]
 	return words
 
-# everyword = create_dict(open_dictionary())
-
+everyword = create_dict(open_dictionary())
+print everyword['POTATO']
 # for key in everyword:
 # 	print key
 # 	print everyword[key] 
+
+
+#WTF KIND OF A DICTIONARY DOESN'T HAVE THE WORD POTATO???????
+def open_json_dictionary():
+	definitions = {}
+	with open('dictionary/dictionary.json') as json_data:
+		definitions = json.load(json_data)
+		json_data.close()
+	return definitions
+
+
+#pprint(open_json_dictionary()["YIELD"])
 
